e8e8f77bd3aa887ac9d09158edabbec02cc3d66c
Enzo Frantz <enzo.frantz@goeuro.com>
2020-04-25 15:35:59 +0200

    temporarily commit build

---
+++ frontend/build/precache-manifest.b2f2649382163c652da1755134b963ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7b6cf140009dbd9c926f09b00f24bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8d00cf3526b7ab00ea9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8f86d0db019a1bfe4998",
+    "url": "/static/js/2.6fe27642.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6fe27642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8d00cf3526b7ab00ea9",
+    "url": "/static/js/main.72cf200a.chunk.js"
+  },
+  {
+    "revision": "bbc8b30b1536c414b496",
+    "url": "/static/js/runtime-main.c3b37fb5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
